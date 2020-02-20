be23d7c05ac87ae496e8a8c79a2ce30ba9e7eae7
John Travers <johnk.travers@gmail.com>
2020-02-20 09:45:04 -0700

    Add top word and word count to right side of page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ document.addEventListener("DOMContentLoaded", async function(event) {
     let topWordData = await response.json();
     let topWord = await Object.keys(topWordData.word)[0];
 
+    document.querySelector('#word').innerHTML = topWord;
+    document.querySelector('#count').innerHTML = topWordData.word[topWord];
+
   } catch(err) {
     console.log(err);
   }
