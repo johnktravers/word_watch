f9d5c3cf7599c5f2348d03a69c4a2cde7cdd159d
John Travers <johnk.travers@gmail.com>
2020-02-20 10:58:49 -0700

    Add postWord function to send a post request to word watch API

--- src/index.js
+++ src/index.js
@@ -13,3 +13,16 @@ document.addEventListener("DOMContentLoaded", async function(event) {
   }
 
 });
+async function postWord(word) {
+  let url = 'https://wordwatch-api.herokuapp.com/api/v1/words';
+  let data = { "word": { "value": word } };
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  });
+
+  return await response.json();
+}
+
