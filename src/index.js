1da79704999aa3ccb3c1d07740d54081d3f47ade
John Travers <johnk.travers@gmail.com>
2020-02-20 11:02:04 -0700

    Add button click event handler to send post request with each input word

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 document.addEventListener("DOMContentLoaded", async function(event) {
+  let topWord = await getTopWord();
 
+  var button = document.querySelector('button');
+
+  button.onclick = async function(event) {
+    let input = document.querySelector('#input').value;
+    let words = input.split(' ');
+
+    words.forEach(async (word) => {
+      if (onlyAlpha(word)) {
+        await postWord(word);
+      }
+    })
+  };
+
+});
+
+
+async function getTopWord() {
   try {
     let response = await fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word');
     let topWordData = await response.json();
@@ -7,12 +25,13 @@ document.addEventListener("DOMContentLoaded", async function(event) {
 
     document.querySelector('#word').innerHTML = topWord;
     document.querySelector('#count').innerHTML = topWordData.word[topWord];
+    return topWord;
 
   } catch(err) {
     console.log(err);
   }
+};
 
-});
 async function postWord(word) {
   let url = 'https://wordwatch-api.herokuapp.com/api/v1/words';
   let data = { "word": { "value": word } };
@@ -26,3 +45,6 @@ async function postWord(word) {
   return await response.json();
 }
 
+function onlyAlpha(word) {
+  return /^[a-zA-Z]+$/.test(word);
+}
