58d02b028a2720f24ba93dac836412c0e3425636
John Travers <johnk.travers@gmail.com>
2020-02-20 09:44:16 -0700

    Add fetch call to word watch API to get top word and word count

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import $ from 'jquery'
+document.addEventListener("DOMContentLoaded", async function(event) {
 
-$(document).ready(() => {
-  // have fun!
-})
+  try {
+    let response = await fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word');
+    let topWordData = await response.json();
+    let topWord = await Object.keys(topWordData.word)[0];
+
+  } catch(err) {
+    console.log(err);
+  }
+
+});
